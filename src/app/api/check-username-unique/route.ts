cbffe8eebeba58f5def3eae65c3c5801abdc0fca
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2025-01-14 22:50:07 +0530

    api to check unique username complete

---
+++ src/app/api/check-username-unique/route.ts
@@ -0,0 +1,78 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+
+import {z} from "zod";
+
+
+import { usernameValidation } from "@/schemas/signUpSchema";
+
+
+
+
+
+const userNameQuerySchema=z.object({
+    username:usernameValidation
+})
+
+
+
+
+export async function GET(request:Request)
+{
+    await dbConnect();
+    try{
+            const {searchParams}=new URL(request.url)
+            const queryParam={
+                username:searchParams.get("username")
+            }
+            const result=userNameQuerySchema.safeParse(queryParam)
+
+            if(!result.success)
+            {
+                
+
+                return Response.json({
+
+                    success:false,
+                    message:"Invalid username",
+                     
+                },{
+                    status:400
+                })
+            }
+
+            const {username}=result.data
+            const existingVerifieduser=await UserModel.findOne({username,isVerified:true})
+
+            if(existingVerifieduser)
+            {
+                return Response.json({
+                    success:false,
+                    message:"Username already exists"
+                },
+                {
+                    status:400
+                })
+            }
+            return Response.json({
+                success:true,
+                message:"Username is available"
+            })
+    }
+    catch(e)
+    {
+        console.log(e)
+        return Response.json({
+            success:false,
+            message:"Error in checking username"
+           
+        },
+        {
+                   status:500
+        }
+    
+    )
+    }
+
+    
+}
